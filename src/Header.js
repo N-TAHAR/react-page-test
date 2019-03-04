cc7043e44a4782e6ff3dcdb223ad9b3313386d77
N.TAHAR <najibtaharberrabah@gmail.com>
2019-03-04 23:11:15 +0100

    header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => (
+  <header>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </header>
+)
+
+export default Header
